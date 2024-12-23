622c12f0f816853ffff71e634413d301d4bd9c54
daavidd09 <davidgaita765@gmail.com>
2024-12-23 23:59:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
+\document.addEventListener('DOMContentLoaded', function () {
     emailjs.init('sMNDwueuZxnqBhT13');
-
+    
     const cotizarButton = document.getElementById('solicitar-cotizare');
+    const modal = document.getElementById('cotizare');
+    const closeModal = document.querySelector('.close');
+    
     cotizarButton.addEventListener('click', function () {
-        // Mostrar la ventana modal cuando se hace clic en el botón
-        document.getElementById('cotizare-modal').style.display = 'flex'; /* Mostrar el modal */
+        modal.style.display = 'flex';
     });
-
-    const closeButton = document.getElementsByClassName('close')[0];
-    closeButton.addEventListener('click', function () {
-        // Cerrar el modal cuando se hace clic en el botón de cerrar
-        document.getElementById('cotizare-modal').style.display = 'none';
+    
+    closeModal.addEventListener('click', function () {
+        modal.style.display = 'none';
     });
 
     const cotizacionForm = document.getElementById('cotizare-form');
     cotizacionForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-
+        e.preventDefault(); 
         const nume = document.getElementById('nume').value;
         const telefon = document.getElementById('telefon').value;
         const email = document.getElementById('email').value;
-
-        // Actualizamos la tabla en la ventana modal con los datos ingresados
-        document.getElementById('modal-nume').textContent = nume;
-        document.getElementById('modal-telefon').textContent = telefon;
-        document.getElementById('modal-email').textContent = email;
-
+        
         const templateParams = {
             nume: nume,
             telefon: telefon,
             email: email,
         };
-
+        
         emailjs.send('service_otwgjg9', 'template_096i836', templateParams)
             .then(function (response) {
                 alert('Solicitarea ta a fost trimisă cu succes! Te vom contacta în curând.');
                 cotizacionForm.reset();
-                document.getElementById('cotizare-modal').style.display = 'none'; /* Ocultar la modal después de enviar */
+                modal.style.display = 'none';
             }, function (error) {
                 alert('A apărut o eroare. Te rugăm să încerci din nou mai târziu.');
             });

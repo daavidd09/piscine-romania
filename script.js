ada4d869d0053294541fb9b752bce3ccefda5fbf
daavidd09 <davidgaita765@gmail.com>
2024-12-23 23:40:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     emailjs.init('sMNDwueuZxnqBhT13');
 
-    
     const cotizarButton = document.getElementById('solicitar-cotizare');
-    
-    
     cotizarButton.addEventListener('click', function () {
         document.getElementById('cotizare').style.display = 'block';
     });
+
     const cotizacionForm = document.getElementById('cotizare-form');
     cotizacionForm.addEventListener('submit', function (e) {
         e.preventDefault(); 
+
         const nume = document.getElementById('nume').value;
         const telefon = document.getElementById('telefon').value;
         const email = document.getElementById('email').value;
+
         const templateParams = {
             nume: nume,
             telefon: telefon,
             email: email,
         };
-         emailjs.send('service_otwgjg9', 'template_096i836', templateParams)
+
+        emailjs.send('service_otwgjg9', 'template_096i836', templateParams)
             .then(function (response) {
                 alert('Solicitarea ta a fost trimisă cu succes! Te vom contacta în curând.');
                 cotizacionForm.reset();
